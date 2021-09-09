146b7af2860cc611617ba5bd96d590c9fe3cdb90
Samuel Kajava <kajavasamuel@gmail.com>
2021-09-09 22:55:24 +0200

    added method to test

--- src/main/java/backend/DataHandler.java
+++ src/main/java/backend/DataHandler.java
@@ -12,6 +12,10 @@ public class DataHandler {
         setProducts(new ProductJsonFileParser());
     }
 
+    public int getSize() {
+        return this.products.size();
+    }
+
     private void setProducts(IProductParser parser) {
         products = parser.getProducts();
     }
