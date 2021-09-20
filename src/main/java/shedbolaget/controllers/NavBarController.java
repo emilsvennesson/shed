1ab3b22b4994e7469266e4cbd0778c9c8089514d
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-09-20 15:28:26 +0200

    created navbar view

---
+++ src/main/java/shedbolaget/controllers/NavBarController.java
@@ -0,0 +1,25 @@
+package shedbolaget.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+public class NavBarController {
+
+    @FXML
+    private MenuButton productsButton;
+
+    @FXML
+    private Button apkLeaderboardButton;
+
+    @FXML
+    private Button drinkGeneratorButton;
+
+    @FXML
+    private Button favoritesButton;
+
+    @FXML
+    private TextField searchTextField;
+
+}
+
