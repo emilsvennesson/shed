9318c8f99465d16996dace6205beaf3775b021a6
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-10-13 12:07:35 +0200

    100 % coverage on CategoryFilter

--- src/test/java/shedbolaget/model/filter/TestCategoryFilter.java
+++ src/test/java/shedbolaget/model/filter/TestCategoryFilter.java
@@ -8,18 +8,29 @@ import shedbolaget.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class TestCategoryFilter {
+    private Model model = Model.getInstance();
+
     @Test
-    public void testGetFilteredProducts() {
-        Model model = Model.getInstance();
+    public void testGetFilteredProductsLevel2() {
         List<Category> categories = new ArrayList<>();
         categories.add(new Category("Ale", 2));
         List<Product> products = model.getAllProducts();
         List<Product> filteredProductsList = CategoryFilter.getFilteredProducts(products, categories);
         for (Product p : filteredProductsList) {
-            Assert.assertTrue(Objects.equals(p.getCategoryLevel2().getName(), "Ale"));
+            Assert.assertEquals("Ale", p.getCategoryLevel2().getName());
+        }
+    }
+
+    @Test
+    public void testGetFilteredProductsLevel1() {
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Öl", 1));
+        List<Product> products = model.getAllProducts();
+        List<Product> filteredProductsList = CategoryFilter.getFilteredProducts(products, categories);
+        for (Product p : filteredProductsList) {
+            Assert.assertEquals("Öl", p.getCategoryLevel1().getName());
         }
     }
 
