9ae05a1243db63b135ec7c56778d97568f8ab35b
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-14 09:28:34 +0200

    Created the IDrinkLoader class, added methods from uml, added javaDoc

---
+++ src/main/java/shedbolaget/model/drinkfeatures/IDrinkLoader.java
@@ -0,0 +1,36 @@
+package shedbolaget.model.drinkfeatures;
+
+import shedbolaget.model.favorites.SavableProductIdList;
+
+import java.util.List;
+
+/**
+ * model.drinkfeatures.IDrinkLoader
+ *
+ * <p> Interface represanting a data reader that is responsible for reading in {@link Drink}s
+ *      This Interface will be used as a common interface for classes reading {@link Drink} data from different data storages
+ *      The attributes that will be read in are
+ *      </p>
+ *      <ul>
+ *  *       <li>Name            String</li>
+ *  *       <li>Ingredients     Multiple of Strings</li>
+ *  *       <li>Steps           Multiple Strings</li>
+ *  *   </ul>
+ *
+ * @author Daniel Rygaard
+ * @version %I%, %G%
+ */
+public interface IDrinkLoader {
+
+
+    /**
+     * Parses information from a set data storage. This method will then instantiate
+     * {@link Drink} objects and set that information into them.
+     *
+     * @return
+     */
+    public List<Drink> load();
+
+
+
+}
