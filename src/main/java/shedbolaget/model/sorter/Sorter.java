04b380e8e2cf8452e2a2bd6cea644b5b216edf9b
KajavaScript <kajavasamuel@gmail.com>
2021-10-14 12:22:32 +0200

    rename Sorter -> ProductListSorter and create facade named Sorter

--- src/main/java/shedbolaget/model/sorter/Sorter.java
+++ src/main/java/shedbolaget/model/sorter/Sorter.java
@@ -2,120 +2,31 @@ package shedbolaget.model.sorter;
 
 import shedbolaget.model.Product;
 
-import java.util.*;
-import java.util.function.Function;
+import java.util.List;
 
 /**
- * The Sorter, used for sorting variables of different types in a list of Products.
- * @author samkaj
+ * Façade for handling logic in ProductListSorter, in order to easily sort lists of products without specifying a Function as parameter.
+ * @author Samuel Kajava
  */
-public final class Sorter {
-    /**
-     * Sort a list of products based on properties of type double.
-     *
-     * @param functionToApply the function to apply
-     * @param products        the list of products
-     */
-    public static void sortProductsDouble(Function<Product, Double> functionToApply, List<Product> products) {
-        products.sort(Comparator.comparingDouble(functionToApply::apply));
+public class Sorter {
+    private Sorter() {
     }
 
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a double which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @return the sorted result of the original list as a new list
-     */
-    public static List<Product> getProductsSortedByDouble(Function<Product, Double> sortingParameter, List<Product> productListToSort) {
-        List<Product> sortedList = new ArrayList<>(productListToSort);
-        sortedList.sort(Comparator.comparingDouble(sortingParameter::apply));
-        return sortedList;
+
+    public static List<Product> getProductListSortedByPrice(List<Product> productList) {
+        return ProductListSorter.getProductsSortedByDouble(Product::getPrice, productList);
     }
 
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a double which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @param reversed          boolean which determines if the returned list should be in reverse order or not
-     * @return the sorted result of the original list as a new list
-     */
-    public static List<Product> getProductsSortedByDouble(Function<Product, Double> sortingParameter, List<Product> productListToSort, boolean reversed) {
-        List<Product> sortedList = getProductsSortedByDouble(sortingParameter, productListToSort);
-        if(reversed)
-            Collections.reverse(sortedList);
-        return sortedList;
+    public static List<Product> getProductListSortedByPrice(List<Product> productList, boolean reversed) {
+        return ProductListSorter.getProductsSortedByDouble(Product::getPrice, productList, reversed);
     }
 
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a boolean which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @return the sorted result of the original list as a new list where True values are first
-     */
-    public static List<Product> getProductsSortedByBoolean(Function<Product, Boolean> sortingParameter, List<Product> productListToSort) {
-        List<Product> sortedList = new ArrayList<>(productListToSort);
-        sortedList.sort(Comparator.comparing(sortingParameter));
-        Collections.reverse(sortedList); // sort by true first
-        return sortedList;
+    public static List<Product> getProductListSortedByApk(List<Product> productList) {
+        return ProductListSorter.getProductsSortedByDouble(Product::getApk, productList);
     }
 
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a boolean which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @param reversed          boolean which determines if the returned list should be in reverse order or not
-     * @return the sorted result of the original list as a new list
-     */
-    public static List<Product> getProductsSortedByBoolean(Function<Product, Boolean> sortingParameter, List<Product> productListToSort, boolean reversed) {
-        List<Product> sortedList = getProductsSortedByBoolean(sortingParameter, productListToSort);
-        if(reversed)
-            Collections.reverse(sortedList);
-        return sortedList;
+    public static List<Product> getProductListSortedByApk(List<Product> productList, boolean reversed) {
+        return ProductListSorter.getProductsSortedByDouble(Product::getApk, productList, reversed);
     }
 
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a String which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @return the sorted result of the original list as a new list
-     */
-    public static List<Product> getProductsSortedByString(Function<Product, String> sortingParameter, List<Product> productListToSort) {
-        List<Product> sortedList = new ArrayList<>(productListToSort);
-        sortedList.sort(Comparator.comparing(sortingParameter));
-        return sortedList;
-    }
-
-    /**
-     * Sort a list based on a given parameter and return the sorted version.
-     * @param sortingParameter  function that returns a String which the sorting will be based upon, i.e. a getter.
-     * @param productListToSort the list of products to sort
-     * @param reversed          boolean which determines if the returned list should be in reverse order or not
-     * @return the sorted result of the original list as a new list
-     */
-    public static List<Product> getProductsSortedByString(Function<Product, String> sortingParameter, List<Product> productListToSort, boolean reversed) {
-        List<Product> sortedList = getProductsSortedByString(sortingParameter, productListToSort);
-        if(reversed)
-            Collections.reverse(sortedList);
-        return sortedList;
-    }
-
-    /**
-     * Sort a list of products based on properties of type boolean.
-     *
-     * @param functionToApply the function to apply
-     * @param products        the list of products
-     */
-    public static void sortProductsBoolean(Function<Product, Boolean> functionToApply, List<Product> products) {
-        products.sort(Comparator.comparing(functionToApply).reversed());
-    }
-
-    /**
-     * Sort a list of products based on properties of type string in alphabetical order.
-     *
-     * @param functionToApply the function to apply
-     * @param products        the list of products
-     */
-    public static void sortProductsString(Function<Product, String> functionToApply, List<Product> products) {
-        products.sort(Comparator.comparing(functionToApply));
-    }
 }
