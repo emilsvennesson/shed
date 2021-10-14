c2e3b53a10c180dfbaa3222b6ea05e827a670978
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-14 09:26:54 +0200

    Cerated the utility class ingredients

---
+++ src/main/java/shedbolaget/model/drinkfeatures/Ingredient.java
@@ -0,0 +1,31 @@
+package shedbolaget.model.drinkfeatures;
+
+import shedbolaget.model.Product;
+import shedbolaget.model.favorites.SavableProductIdList;
+
+/**
+ * model.drinkfeatures.Ingredient
+ *
+ * <p> Used as a utility class and represents a ingredient
+ *     The responsibility of this class is to represent a product as an ingredient</p>
+ *
+ * @author Daniel Rygaard
+ * @version %I%, %G%
+ */
+public class Ingredient {
+
+
+    String ingredient;
+
+    public Ingredient(Product prod){
+
+        //TODO turn the product into ingredient
+
+
+    }
+
+
+    public String getIngredient() {
+        return ingredient;
+    }
+}
