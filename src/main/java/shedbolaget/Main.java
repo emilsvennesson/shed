6f8827c11e2873e73a6f170ff40f551e4e357e44
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-10-15 18:42:36 +0200

    MainLauncher -> JavaFXInitalizer

--- src/main/java/shedbolaget/Main.java
+++ src/main/java/shedbolaget/Main.java
@@ -1,20 +1,9 @@
 package shedbolaget;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+public class Main {
 
-public class Main extends Application {
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = new FXMLLoader(getClass().getResource("/fxml/RootView.fxml")).load();
-        primaryStage.setTitle("Shedbolaget.");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        // workaround to allow loading javafx from class path
+        JavaFXInitializer.main(args);
     }
 }
