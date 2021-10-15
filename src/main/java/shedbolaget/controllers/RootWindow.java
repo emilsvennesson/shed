1b5af8df9028d64fa017556f8f0981e1d0ab9cd8
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-10-15 16:21:47 +0200

    add NavigationEvent

--- src/main/java/shedbolaget/controllers/RootWindow.java
+++ src/main/java/shedbolaget/controllers/RootWindow.java
@@ -1,8 +1,11 @@
 package shedbolaget.controllers;
 
+import com.google.common.eventbus.Subscribe;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import shedbolaget.controllers.components.ComponentFactory;
+import shedbolaget.model.events.EventManager;
+import shedbolaget.model.events.NavigationEvent;
 
 public class RootWindow {
 
@@ -17,7 +20,30 @@ public class RootWindow {
 
     @FXML
     public void initialize() {
+        EventManager.getInstance().registerToEventBus(this);
         this.navBarPane.getChildren().add(navBar);
-        this.contentWrapperAnchorPane.getChildren().add(productsPage);
+        this.contentWrapperAnchorPane.getChildren().add(mainPage);
+    }
+
+    public void openProductsPage() {
+        contentWrapperAnchorPane.getChildren().clear();
+        contentWrapperAnchorPane.getChildren().add(productsPage);
+    }
+
+    public void openMainPage() {
+        contentWrapperAnchorPane.getChildren().clear();
+        contentWrapperAnchorPane.getChildren().add(mainPage);
+    }
+
+    @Subscribe
+    public void onNavigationEvent(NavigationEvent event) {
+        switch (event.getPageToNavigateTo()) {
+            case PRODUCTS:
+                openProductsPage();
+                break;
+            case MAIN:
+                openMainPage();
+                break;
+        }
     }
 }
