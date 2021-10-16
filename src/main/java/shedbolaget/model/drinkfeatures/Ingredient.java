d0ff2d42b6861b999be457c22479e6d9b627e580
Danryg <danryg@studetn.chalmers.se>
2021-10-16 17:56:20 +0200

    Hard coding the ingredient class.

--- src/main/java/shedbolaget/model/drinkfeatures/Ingredient.java
+++ src/main/java/shedbolaget/model/drinkfeatures/Ingredient.java
@@ -19,24 +19,33 @@ public class Ingredient {
 
 
     private String ingredient;
+    private Product product;
 
     public Ingredient(Product prod){
-
+        this.product = prod;
         //TODO turn the product into ingredient
         if(prod.getCategoryLevel1().equals("sprit")){
             if(prod.getCategoryLevel2().equals("rom")){
+                //TODO test if this equals works
                 if(prod.getCategoryLevel3().equals("M\\u00f6rk rom")){
-                    this.ingredient = "Dark Rum";
+                    this.ingredient = "Dark rum";
                 }
-                if(prod.getCategoryLevel3().equals("M\\u00f6rk rom")){
+                if(prod.getCategoryLevel3().equals("Light rum")){
                     this.ingredient = "Light Rum";
                 }
             }
 
-            if(prod.getCategoryLevel2().equals("vodka")){
-
+            if(prod.getCategoryLevel2().equals("Vodka")){
+                this.ingredient = "Vodka";
 
             }
+
+            if(prod.getCategoryLevel2().equals("Tequila & Mezcal")){
+                this.ingredient = "Tequila";
+            }
+
+
+
         }
     }
 
