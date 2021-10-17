c66a62a1b6acc345c48bafedbff8884a1b76795d
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-17 21:35:08 +0200

    Added some tests for the DrinkModel

--- src/test/java/shedbolaget/model/drinkfeatures/TestDrinkModel.java
+++ src/test/java/shedbolaget/model/drinkfeatures/TestDrinkModel.java
@@ -1,11 +1,31 @@
-package java.shedbolaget.model.drinkfeatures;
+package shedbolaget.model.drinkfeatures;
 
 import org.junit.Test;
 import shedbolaget.model.drinkfeatures.DrinkModel;
 
+import java.util.List;
+
 public class TestDrinkModel {
 
     DrinkModel dModel = new DrinkModel();
+
+
+    @Test
+    public void parsing(){
+        DrinkJsonParser parser = new DrinkJsonParser();
+        List<Drink> hej = parser.load();
+
+        for (Drink d :
+                hej) {
+            System.out.println(d.getName());
+        }
+
+    }
+
+    @Test
+    public void removeIngredient(){
+
+    }
 
 
     @Test
