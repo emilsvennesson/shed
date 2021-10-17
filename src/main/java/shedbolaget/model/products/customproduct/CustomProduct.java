a7e737c5f5ae8aa95f6ebffae2b4a6fce38e8680
Kexon <kexon@hotmail.se>
2021-10-17 15:53:02 +0200

    Update CustomProduct.java

--- src/main/java/shedbolaget/model/products/customproduct/CustomProduct.java
+++ src/main/java/shedbolaget/model/products/customproduct/CustomProduct.java
@@ -14,46 +14,23 @@ public class CustomProduct {
     public static final String CUSTOM_PRODUCTS_FILENAME = "customproducts.json";
     public static void createProduct(String name, String category1, String category2, double price, double volume, int alcoholPercentage)
     {
+        // create Product object
+        Product newCustomProduct = new Product(name, category1, category2, price, volume, alcoholPercentage);
+        writeProductToJsonFile(newCustomProduct);
+    }
 
+    private static void writeProductToJsonFile(Product product){
         try {
-            // create Product object
-            Product newCustomProduct = new Product(name, category1, category2, price, volume, alcoholPercentage);
-
             // create object mapper instance
             ObjectMapper mapper = new ObjectMapper();
             mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
             mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
 
             // convert book object to JSON file
-            mapper.writeValue(Paths.get("src", "main", "resources", CUSTOM_PRODUCTS_FILENAME).toFile(), newCustomProduct);
+            mapper.writeValue(Paths.get("src", "main", "resources", CUSTOM_PRODUCTS_FILENAME).toFile(), product);
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        /*
-        try {
-            // https://attacomsian.com/blog/jackson-write-json-file
-            // create a map
-            Map<String, Object> map = new HashMap<>();
-            map.put("productId", "0");
-            map.put("productNameBold", name);
-            map.put("productNameThin", "null");
-            map.put("volumeText", volume + " ml");
-            map.put("volume", volume);
-            map.put("price", price);
-            map.put("categoryLevel1", category1);
-            map.put("categoryLevel2", category2);
-
-            // create object mapper instance
-            ObjectMapper mapper = new ObjectMapper();
-
-            // convert map to JSON file
-            mapper.writeValue(Paths.get("customproducts.json").toFile(), map);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-         */
-
     }
 }
