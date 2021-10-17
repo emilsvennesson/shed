aa908abdc30fcd4d9c9f264773ba73d68ead8283
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-18 00:48:24 +0200

    Added some tests for the drink model

--- src/test/java/shedbolaget/model/drinkfeatures/TestDrinkModel.java
+++ src/test/java/shedbolaget/model/drinkfeatures/TestDrinkModel.java
@@ -1,8 +1,16 @@
-package shedbolaget.model.drinkfeatures;
+package shedbolaget.model.drinks;
 
+import org.junit.Assert;
 import org.junit.Test;
-import shedbolaget.model.drinkfeatures.DrinkModel;
+import shedbolaget.model.categories.Category;
+import shedbolaget.model.products.Product;
+import shedbolaget.model.products.ProductsHolder;
+import shedbolaget.model.products.filter.Filter;
 
+import shedbolaget.model.drinks.parser.DrinkJsonFileParser;
+import shedbolaget.model.drinks.parser.IDrinkParser;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TestDrinkModel {
@@ -12,7 +20,7 @@ public class TestDrinkModel {
 
     @Test
     public void parsing(){
-        DrinkJsonParser parser = new DrinkJsonParser();
+        IDrinkParser parser = new DrinkJsonFileParser();
         List<Drink> hej = parser.load();
 
         for (Drink d :
@@ -23,14 +31,47 @@ public class TestDrinkModel {
     }
 
     @Test
-    public void removeIngredient(){
+    public void canFindGinDrinkWithEveryGinProduct(){
+
+
+        List<Product> prods = Filter.search(ProductsHolder.getInstance().getAllProducts(), "Gin", 100);
+
+        Assert.assertNotEquals(prods.size(), 0);
+
+        for (Product p :
+                prods) {
+
+            dModel.reset();
+            dModel.addIngredient(p);
+            List<Drink> din = dModel.loadDrinks();
+
+            Assert.assertNotEquals(din.size(), 0);
+
+
+        }
+
+        List<Drink> din = dModel.loadDrinks();
+        for (Drink d:
+             din) {
+            System.out.println(d.getName());
+        }
+
+
+
+
+
 
     }
 
 
-    @Test
-    public void addIngredient(){
 
+    public void addIngredient(){
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("Tequila", 2));
+        List<Product> products = ProductsHolder.getInstance().getAllProducts();
+        List<Product> filteredProductsList = Filter.getFilteredProductsByCategory(products, categories);
+
+        System.out.println(filteredProductsList.get(0).getProductNameBold());
 
 
     }
