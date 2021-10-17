278821cdb3c07f99fcdac6714545f4253981046d
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-10-17 15:31:58 +0200

    cleanup

--- src/main/java/shedbolaget/model/products/filter/ProductsSearch.java
+++ src/main/java/shedbolaget/model/products/filter/ProductsSearch.java
@@ -1,30 +1,45 @@
 package shedbolaget.model.products.filter;
 
 import me.xdrop.fuzzywuzzy.FuzzySearch;
-import me.xdrop.fuzzywuzzy.model.ExtractedResult;
 import shedbolaget.model.products.Product;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 enum ProductsSearch {
     ;
 
-    static List<Product> searchByName(List<Product> products, String query) {
-        List<ExtractedResult> result = FuzzySearch.extractSorted(query, extractFromMethod(Product::getFullProductName, products));
-        List<Product> matches = new ArrayList<>();
-        for (ExtractedResult r : result)
-            matches.add(products.get(r.getIndex()));
-        return matches;
+    static List<Product> search(List<Product> products, String query, int requiredHitRatio) {
+        Map<Product, Integer> productsNameHit = getNameHitRatio(products, query, requiredHitRatio);
+        Map<Product, Integer> productsCategoryHit = getCategoryHitRatio(products, query, requiredHitRatio);
+        HashMap<Product, Integer> mergedMap =
+                productsCategoryHit.entrySet()
+                        .stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (v1, v2) -> v1 > v2 ? v1 : v2, () -> new HashMap<>(productsNameHit)));
+        return new ArrayList<>(sortMap(mergedMap).keySet());
     }
 
-    static List<Product> searchByCategory(List<Product> products, String query) {
-        Map<Product, Integer> pScore = getCategoryHitRatio(products, query);
+    static List<Product> searchByName(List<Product> products, String query, int requiredHitRatio) {
+        Map<Product, Integer> pScore = getNameHitRatio(products, query, requiredHitRatio);
         return new ArrayList<>(sortMap(pScore).keySet());
     }
 
-    static Map<Product, Integer> getCategoryHitRatio(List<Product> products, String query) {
+    static List<Product> searchByCategory(List<Product> products, String query, int requiredHitRatio) {
+        Map<Product, Integer> pScore = getCategoryHitRatio(products, query, requiredHitRatio);
+        return new ArrayList<>(sortMap(pScore).keySet());
+    }
+
+    private static Map<Product, Integer> getNameHitRatio(List<Product> products, String query, int requiredHitRatio) {
+        Map<Product, Integer> pScore = new LinkedHashMap<>();
+        for (Product product : products) {
+            int score = FuzzySearch.ratio(query, product.getFullProductName());
+            if (score >= requiredHitRatio)
+                pScore.put(product, score);
+        }
+        return pScore;
+    }
+
+    private static Map<Product, Integer> getCategoryHitRatio(List<Product> products, String query, int requiredHitRatio) {
         Map<Product, Integer> pScore = new LinkedHashMap<>();
         for (Product product : products) {
             int r1 = FuzzySearch.ratio(query, product.getCategoryLevel1().getName());
@@ -35,13 +50,14 @@ enum ProductsSearch {
             } catch (NullPointerException ignored) {
             }
             int score = Math.max(r1, Math.max(r2, r3));
-            pScore.put(product, score);
+            if (score >= requiredHitRatio)
+                pScore.put(product, score);
         }
         return pScore;
     }
 
-    static Map<Product, Integer> sortMap(Map<Product, Integer> pMap) {
-        Map<Product, Integer> sortedMap = pMap.entrySet().stream()
+    private static Map<Product, Integer> sortMap(Map<Product, Integer> pMap) {
+        return pMap.entrySet().stream()
                 .sorted(Comparator.comparingInt(e -> -e.getValue()))
                 .collect(Collectors.toMap(
                         Map.Entry::getKey,
@@ -51,14 +67,6 @@ enum ProductsSearch {
                         },
                         LinkedHashMap::new
                 ));
-
-        return sortedMap;
-    }
-
-    static List<String> extractFromMethod(Function<Product, String> stringFunction, List<Product> productList) {
-        List<String> stringList = new ArrayList<>();
-        productList.forEach(p -> stringList.add(stringFunction.apply(p)));
-        return stringList;
     }
 
 }
