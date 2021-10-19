b551fb906c307b0d557d02201a5ab5c790f3e7bc
Samuel Kajava <kajavasamuel@gmail.com>
2021-10-19 23:20:38 +0200

    update APK Top 3 controller, now displays APK

--- src/main/java/shedbolaget/controllers/components/APKTop3Component.java
+++ src/main/java/shedbolaget/controllers/components/APKTop3Component.java
@@ -21,46 +21,59 @@ public class APKTop3Component extends Component {
     private VBox firstPlaceVBox;
     @FXML
     private Text firstPlaceInfoText;
+    private Product firstPlaceProduct;
 
     @FXML
     private VBox secondPlaceVBox;
     @FXML
     private Text secondPlaceInfoText;
+    private Product secondPlaceProduct;
 
 
     @FXML
     private VBox thirdPlaceVBox;
     @FXML
     private Text thirdPlaceInfoText;
-
+    private Product thirdPlaceProduct;
 
     protected APKTop3Component() {
         super("APKTop3View");
         List<Product> all = ProductsHolder.getInstance().getAllProducts();
         List<Product> filteredList = Filter.getFilteredProductsByCategory(Sorter.getProductListSortedByApk(all), Categories.getLevel1Categories(all));
         populateTop3(filteredList);
+        eventManager.registerToEventBus(this);
     }
 
     private void populateTop3(List<Product> products) {
-        secondPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(products.get(0)));
-        firstPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(products.get(1)));
-        thirdPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(products.get(2)));
+        firstPlaceProduct = products.get(0);
+        secondPlaceProduct = products.get(1);
+        thirdPlaceProduct = products.get(2);
+
+        secondPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(firstPlaceProduct));
+        firstPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(secondPlaceProduct));
+        thirdPlaceVBox.getChildren().add(1, ComponentFactory.createBasicProductCard(thirdPlaceProduct));
+        updateInfoText();
     }
 
     private void loadTop3(List<Product> products) {
         secondPlaceVBox.getChildren().remove(1);
         firstPlaceVBox.getChildren().remove(1);
         thirdPlaceVBox.getChildren().remove(1);
-        System.out.println(products.get(0).getProductNameBold());
         populateTop3(products);
+    }
+
+    private void updateInfoText() {
+        firstPlaceInfoText.setText(String.format("APK: %.2f", firstPlaceProduct.getApk()));
+        secondPlaceInfoText.setText(String.format("APK: %.2f", secondPlaceProduct.getApk()));
+        thirdPlaceInfoText.setText(String.format("APK: %.2f", thirdPlaceProduct.getApk()));
     }
 
     @Subscribe
     private void actOnCategoryEvent(CategoryEvent event) {
         ProductsHolder productsHolder = ProductsHolder.getInstance();
-        List<Product> filteredProducts = Filter.getFilteredProductsByCategory(productsHolder.getAllProducts(), event.getActiveCategories());
+        List<Product> filteredProducts = Sorter.getProductListSortedByApk(Filter.getFilteredProductsByCategory(productsHolder.getAllProducts(), event.getActiveCategories()), true);
         if (!filteredProducts.isEmpty())
-            loadTop3(filteredProducts.subList(0,3));
+            loadTop3(filteredProducts.subList(0, 3));
     }
 
 
