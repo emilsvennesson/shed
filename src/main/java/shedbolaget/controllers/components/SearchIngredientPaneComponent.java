3e15880505d1dc31e1e3629095b498fecb44e43a
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-19 14:18:30 +0200

    Added a seperate fxml file and component for the pane that represents the available products that can be added as an ingredient

---
+++ src/main/java/shedbolaget/controllers/components/SearchIngredientPaneComponent.java
@@ -0,0 +1,7 @@
+package shedbolaget.controllers.components;
+
+public class SearchIngredientPaneComponent extends Component {
+    protected SearchIngredientPaneComponent() {
+        super("SearchIngredientPane");
+    }
+}
