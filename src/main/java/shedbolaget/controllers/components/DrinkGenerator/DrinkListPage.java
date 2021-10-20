ffb7d662947a0a2e0c6ffd69e351b2ceb5e77efe
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-20 13:11:15 +0200

    DrinkListPage now renders drinks on a drinkListevent

--- src/main/java/shedbolaget/controllers/components/DrinkGenerator/DrinkListPage.java
+++ src/main/java/shedbolaget/controllers/components/DrinkGenerator/DrinkListPage.java
@@ -1,11 +1,17 @@
 package shedbolaget.controllers.components.DrinkGenerator;
 
+import com.google.common.eventbus.Subscribe;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
 import shedbolaget.controllers.components.Component;
+import shedbolaget.controllers.components.ComponentFactory;
+import shedbolaget.model.drinks.Drink;
+import shedbolaget.model.events.DrinkListEvent;
 import shedbolaget.model.events.EventManager;
 import shedbolaget.model.events.NavigationEvent;
+
+import java.util.List;
 
 public class DrinkListPage extends Component {
 
@@ -18,6 +24,7 @@ public class DrinkListPage extends Component {
 
     public DrinkListPage() {
         super("DrinkListPage");
+        EventManager.getInstance().registerToEventBus(this);
     }
 
 
@@ -27,5 +34,26 @@ public class DrinkListPage extends Component {
         EventManager.getInstance().fireEvent(new NavigationEvent(NavigationEvent.NAVIGATION.DRINKGENERATOR));
 
     }
+
+    void renderDrink(Drink drink){
+
+        DrinkFlowPane.getChildren().add(ComponentFactory.createDrinkCard(drink));
+
+    }
+
+    void renderDrinks(List<Drink> drinks, int amount){
+        DrinkFlowPane.getChildren().clear();
+        for(int i = 0; i< amount && i <drinks.size(); i++){
+            renderDrink(drinks.get(i));
+
+        }
+
+    }
+
+    @Subscribe
+    public void actOnDrinkListEvent(DrinkListEvent event){
+        renderDrinks(event.getDrinks(),30);
+    }
+
 
 }
