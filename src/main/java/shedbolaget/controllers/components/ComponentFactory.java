46a7b0d24c1ed4a9e31c3debe177c33950cba467
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-20 13:10:21 +0200

    Component factory can now create a DrinkCardComponent

--- src/main/java/shedbolaget/controllers/components/ComponentFactory.java
+++ src/main/java/shedbolaget/controllers/components/ComponentFactory.java
@@ -1,9 +1,11 @@
 package shedbolaget.controllers.components;
 
 import javafx.scene.layout.AnchorPane;
+import shedbolaget.controllers.components.DrinkGenerator.DrinkCardComponent;
 import shedbolaget.controllers.components.DrinkGenerator.DrinkGeneratorPage;
 import shedbolaget.controllers.components.DrinkGenerator.DrinkListPage;
 import shedbolaget.controllers.components.DrinkGenerator.IngredientCardComponent;
+import shedbolaget.model.drinks.Drink;
 import shedbolaget.model.drinks.Ingredient;
 import shedbolaget.model.products.Product;
 import shedbolaget.model.products.pages.Pages;
@@ -81,4 +83,10 @@ public class ComponentFactory {
 
         return new DrinkListPage().getPane();
     }
+
+    public static AnchorPane createDrinkCard(Drink drink){
+        return new DrinkCardComponent(drink).getPane();
+    }
+
+
 }
