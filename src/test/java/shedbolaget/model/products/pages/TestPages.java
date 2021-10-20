f57bebe71f82bf93cff86de0372949dab7f92394
emilsvennesson <emilsvennesson@users.noreply.github.com>
2021-10-20 16:27:44 +0200

    more tests for Pages

--- src/test/java/shedbolaget/model/products/pages/TestPages.java
+++ src/test/java/shedbolaget/model/products/pages/TestPages.java
@@ -11,4 +11,21 @@ public class TestPages {
         Pages pages = new Pages(ProductModel.getInstance().getAllProducts());
         Assert.assertEquals(pages.getNumberOfPages(), (int) Math.ceil((double) pages.getTotalNumberOfProducts() / (double) pages.getProductsLimitPerPage()));
     }
+
+    @Test
+    public void testSpecifiedNumberOfPages() {
+        Pages pages = new Pages(ProductModel.getInstance().getAllProducts(), 100);
+        int expectedNumberOfPages = (int) Math.ceil((double) pages.getTotalNumberOfProducts() / (double) 100);
+        Assert.assertEquals(expectedNumberOfPages, pages.getNumberOfPages());
+    }
+
+    @Test
+    public void testPagesBoundaries() {
+        Pages pages = new Pages(ProductModel.getInstance().getAllProducts(), 100);
+        // expect empty list when we are out of bounds
+        Assert.assertEquals(0, pages.getProductsFromPage(pages.getNumberOfPages() + 1).size());
+        Assert.assertEquals(0, pages.getProductsFromPage(0).size());
+        Assert.assertEquals(0, pages.getNumberOfProducts(-1));
+        Assert.assertEquals(0, pages.getNumberOfProducts(pages.getNumberOfPages() + 1));
+    }
 }
