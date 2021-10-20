18ab9d53b4605a0a897b3a0e74c2c570836ecb0d
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2021-10-20 13:09:51 +0200

    Changed how the drink card view looks

--- src/main/java/shedbolaget/controllers/components/DrinkGenerator/DrinkCardComponent.java
+++ src/main/java/shedbolaget/controllers/components/DrinkGenerator/DrinkCardComponent.java
@@ -3,6 +3,7 @@ package shedbolaget.controllers.components.DrinkGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -11,12 +12,14 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import shedbolaget.controllers.components.Component;
 import shedbolaget.model.drinks.Drink;
+import shedbolaget.model.products.Product;
 
 public class DrinkCardComponent extends Component {
     Drink drink;
-    protected DrinkCardComponent(Drink drink) {
+    public DrinkCardComponent(Drink drink) {
         super("DrinkCardView");
         this.drink = drink;
+        initFields();
     }
 
 
@@ -49,8 +52,6 @@ public class DrinkCardComponent extends Component {
     @FXML
     private Text priceText;
 
-    @FXML
-    private Button favoritesButton;
 
     @FXML
     void cardOnClick(MouseEvent event) {
@@ -58,18 +59,22 @@ public class DrinkCardComponent extends Component {
     }
 
     @FXML
-    void favoritesButtonOnClick(ActionEvent event) {
+    void detailsButtonOnClick(ActionEvent event) {
+        //TODO open detailed view
 
     }
 
     /*----------------------------------- Private methods -----------------------------------*/
     private void initFields(){
-        nameBoldText.setText(drink.getName());
-        nameThinText.setText(drink.strGlass);
+        this.nameBoldText.setText(drink.getName());
+        this.nameThinText.setText(drink.strGlass);
 
-        IngredientsCount.setText("Ingredients: " + drink.getAlcoIngredients().size());
-        apkText.setText("APK: " + drink.getAPK());
-        volumeText.setText("Volume: "+drink.getVolume());
+        this.IngredientsCount.setText("Ingredients: " + drink.getAlcoIngredients().size());
+        //this.apkText.setText("APK: " + drink.getAPK());
+        this.volumeText.setText("Volume: "+drink.getVolume());
+
+        Image productImage = new Image(drink.strImageSource, 0, 0, false, false, true);
+        this.imageView.setImage(productImage);
 
     }
 
